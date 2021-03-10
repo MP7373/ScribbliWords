bd25e310e97d3baab55cf28ca3a72b014bc41c48
Martin <martinpayne73@gmail.com>
2021-03-09 22:09:47 -0500

    add Enter key listener, clear text box after word is submitted

--- main.js
+++ main.js
@@ -1,56 +1,92 @@
-firebase.initializeApp({
-  apiKey: "AIzaSyBMRrXzMzuXoUmGHOwtkwqHKZ0RphIOSC4",
-  authDomain: "scribliiowords.firebaseapp.com",
-  projectId: "scribliiowords",
-});
-const firestore = firebase.firestore();
+(() => {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBMRrXzMzuXoUmGHOwtkwqHKZ0RphIOSC4",
+    authDomain: "scribliiowords.firebaseapp.com",
+    projectId: "scribliiowords",
+  });
+  const firestore = firebase.firestore();
+  const collection = firestore.collection("WordsCollection");
+  const wordDocument = collection.doc("WordDocument");
 
-const statusSection = document.getElementById("status");
+  const statusSection = document.getElementById("status");
+  const wordInput = document.getElementById("main-input");
+  const addWordButton = document.getElementById("main-btn");
+  let submittingInProgress = false;
 
-const wordInput = document.getElementById("main-input");
+  addWordButton.addEventListener("click", submitWord);
+  wordInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      submitWord(event);
+    }
+  });
 
-const addWordButton = document.getElementById("main-btn");
-addWordButton.addEventListener("click", (e) => {
-  const newWord = wordInput.value;
+  function submitWord(event) {
+    if (!submittingInProgress) {
+      submittingInProgress = true;
+      const newWord = wordInput.value;
 
-  if (newWord.length > 0) {
-    const collection = firestore.collection("WordsCollection");
-    const wordDocument = collection.doc("WordDocument");
+      if (newWord.length > 0) {
+        wordDocument
+          .get()
+          .then((doc) => {
+            const words = doc.get("Words");
 
-    wordDocument.get().then((doc) => {
-      const words = doc.get("Words");
-
-      if (!words.includes(newWord)) {
-        words.push(newWord);
-        wordDocument.update({ Words: words });
-        updateStatus(newWord);
+            if (!words.includes(newWord)) {
+              words.push(newWord);
+              wordDocument
+                .update({ Words: words })
+                .then(() => displayWordAdded(newWord))
+                .finally(() => {
+                  wordInput.value = "";
+                  submittingInProgress = false;
+                });
+            } else {
+              displayWordFailedToBeAdded(newWord);
+              wordInput.value = "";
+              submittingInProgress = false;
+            }
+          })
+          .catch((error) => {
+            wordInput.value = "";
+            submittingInProgress = false;
+          });
       } else {
-        updateStatusFailed(newWord);
+        displayWordCantBeBlank();
+        wordInput.value = "";
+        submittingInProgress = false;
       }
-    });
-  } else {
-    wordCantBeBlank();
+    }
   }
-});
 
-function updateStatus(word) {
-  statusSection.innerText = word + " added to list.";
-  setTimeout(() => {
-    statusSection.innerText = "";
-  }, 500);
-}
+  let statusDisplayTimeout = null;
+  function displayWordAdded(word) {
+    if (statusDisplayTimeout !== null) {
+      clearTimeout(statusDisplayTimeout);
+    }
+    statusSection.innerText = word + " added to list.";
+    statusDisplayTimeout = setTimeout(() => {
+      statusSection.innerText = "";
+    }, 500);
+  }
 
-function updateStatusFailed(word) {
-  statusSection.innerText =
-    word + " failed to be added to list. May be a duplicate.";
-  setTimeout(() => {
-    statusSection.innerText = "";
-  }, 500);
-}
+  function displayWordFailedToBeAdded(word) {
+    if (statusDisplayTimeout !== null) {
+      clearTimeout(statusDisplayTimeout);
+    }
+    statusSection.innerText =
+      word + " failed to be added to list. May be a duplicate.";
+    statusDisplayTimeout = setTimeout(() => {
+      statusSection.innerText = "";
+    }, 500);
+  }
 
-function wordCantBeBlank() {
-  statusSection.innerText = "Word cannot be blank.";
-  setTimeout(() => {
-    statusSection.innerText = "";
-  }, 500);
-}
+  function displayWordCantBeBlank() {
+    if (statusDisplayTimeout !== null) {
+      clearTimeout(statusDisplayTimeout);
+    }
+    statusSection.innerText = "Word cannot be blank.";
+    statusDisplayTimeout = setTimeout(() => {
+      statusSection.innerText = "";
+    }, 500);
+  }
+})();
